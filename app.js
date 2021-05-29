ca2691b0ac40dec0093db99002bfbdd46a6195bd
reltaher <43248170+reltaher@users.noreply.github.com>
2021-05-28 22:48:02 -0400

    Installed and implemented connect-flash

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const engine = require('ejs-mate');
 const session = require('express-session');
+const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 
@@ -43,6 +44,13 @@ const sessionConfig = {
     }
 }
 app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
 
 app.get('/', (req, res) => {
     res.render('home')
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -25,9 +25,9 @@ router.get('/', catchAsync(async (req, res) => {
  });
  
  router.post('/', validateCampground, catchAsync(async(req, res) => {
- 
      const campground = new Campground(req.body.campground);
-     await campground.save();
+     await campground.save()
+     req.flash('success', 'Successfully made a new campground!')
      res.redirect(`/campgrounds/${campground._id}`)
  }));
  
