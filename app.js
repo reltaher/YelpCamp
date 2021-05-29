11b29a3dc8cab59cda358b1273552d18c46f8f3c
reltaher <43248170+reltaher@users.noreply.github.com>
2021-05-28 21:44:54 -0400

    moved validateForms.js in a directory named 'javascripts'

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('home')
