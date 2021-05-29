087b841784f44007dfcb8a774383c078639961df
reltaher <43248170+reltaher@users.noreply.github.com>
2021-05-28 21:38:33 -0400

    Moved the JS script to validate forms to its own JS file in a public directory

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('home')
