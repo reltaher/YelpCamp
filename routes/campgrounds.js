814cdff0b34eceda04347220edb12614b0ce2fd3
reltaher <43248170+reltaher@users.noreply.github.com>
2021-05-28 23:37:24 -0400

    added a flash error for campgrounds that do not exist that users try to edit

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -42,6 +42,10 @@ router.get('/', catchAsync(async (req, res) => {
  
  router.get('/:id/edit', catchAsync(async(req, res) => {
      const campground = await Campground.findById(req.params.id)
+     if(!campground){
+        req.flash('error', 'Campground does not exist')
+        return res.redirect('/campgrounds');
+    }
      res.render('campgrounds/edit', { campground });
  }));
  
