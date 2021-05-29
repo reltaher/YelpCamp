eceb662991ee009a97d0fd2092f51098e60fdaa3
reltaher <43248170+reltaher@users.noreply.github.com>
2021-05-29 02:45:59 -0400

    required passport and passport local, and added an app.use for initialize and session for passport

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
 
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
@@ -45,6 +47,9 @@ const sessionConfig = {
 }
 app.use(session(sessionConfig));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
