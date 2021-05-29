dbb04f8c9ce8924d92e3d11fd9ef0019101d804a
reltaher <43248170+reltaher@users.noreply.github.com>
2021-05-29 19:57:37 -0400

    Added a route for users; renamed campgrounds and reviews to campgroundRoutes and reviewRoutes

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -66,8 +67,8 @@ app.get('/', (req, res) => {
     res.render('home')
 });
 
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
 
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404))
