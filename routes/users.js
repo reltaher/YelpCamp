741003cb489193289ea5bd7214e4aff39b68d67f
reltaher <43248170+reltaher@users.noreply.github.com>
2021-05-29 23:12:23 -0400

    Added login template and implemented login routes

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 
@@ -24,8 +25,9 @@ router.get('/login', (req, res) => {
     res.render('users/login');
 })
 
-router.post('/login', (req, res) => {
-
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'Welcome Back!');
+    res.redirect('/campgrounds');
 })
 
 module.exports = router;
