56f2757807edb694a2a9fab28189d5b50fa19170
reltaher <43248170+reltaher@users.noreply.github.com>
2021-06-02 16:14:45 -0400

    Added functionality to display the author of each review

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -22,7 +22,12 @@ router.get('/', catchAsync(async (req, res) => {
  }));
  
  router.get('/:id', catchAsync(async(req, res) => {
-     const campground = await Campground.findById(req.params.id).populate('reviews').populate('author');
+     const campground = await Campground.findById(req.params.id).populate({
+         path: 'reviews',
+         populate: {
+             path: 'author'
+         }
+        }).populate('author');
      if(!campground){
          req.flash('error', 'Campground does not exist')
          return res.redirect('/campgrounds');
