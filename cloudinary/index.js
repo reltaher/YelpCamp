9d9d39058ec6b6f113dd7e3f6c4fa93e7e573fa0
reltaher <43248170+reltaher@users.noreply.github.com>
2021-06-03 14:30:07 -0400

    folder and allowedFormats in params object

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -9,8 +9,10 @@ cloundinary.config({
 
 const storage = new CloudinaryStorage({
     cloudinary,
-    folder: 'YelpCamp',
-    allowedFormats: ['jpeg', 'png', 'jpg']
+    params: {
+        folder: 'YelpCamp',
+        allowedFormats: ['jpeg', 'png', 'jpg']
+    }
 });
 
 module.exports = {
