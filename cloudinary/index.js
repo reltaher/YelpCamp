367188c424f5404d0dca040464e3bb45b9e9c8ed
reltaher <43248170+reltaher@users.noreply.github.com>
2021-06-03 14:35:58 -0400

    fix typo

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -1,7 +1,7 @@
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
-cloundinary.config({
+cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_KEY,
     api_secret: process.env.CLOUDINARY_SECRET
